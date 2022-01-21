4231f0c7ac51b394ddd01f5a6f2088791fc5d817
Mitchell Carr <77063411+Mitch114@users.noreply.github.com>
2022-01-20 21:29:09 -0500

    Add files via upload

---
+++ program1-2.js
@@ -0,0 +1,11 @@
+/* Tillman could run 10 kilometers in 55 min and 30 seconds */
+/* Write the code that will display his average speed in MPH */
+
+let kilometers = 10
+let miles = .62137
+let time = .56
+
+mph = kilometers * miles
+mph = Math.round(mph)
+
+console.log(mph, "mph")
